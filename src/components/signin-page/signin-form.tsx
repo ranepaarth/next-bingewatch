a9da1d2e0c56d484a4483356ff89b144e078cad5
ranepaarth <paarth.medisage@outlook.com>
2024-09-14 22:09:09 +0530

    implement sign-in page

---
+++ src/components/signin-page/signin-form.tsx
@@ -0,0 +1,45 @@
+import React, { FormEvent } from "react";
+import Input from "../reusable/custom-input";
+import ButtonPrimary from "../reusable/button-primary";
+import Link from "next/link";
+
+const SignInForm = () => {
+  const handleSignIn = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="bg-black/65 w-full max-w-[450px] py-8 px-14 rounded-sm">
+      <p className="text-white font-semibold text-3xl">Sign In</p>
+      <div className="py-8">
+        <form action="" className="flex gap-4 flex-col" onSubmit={handleSignIn}>
+          <Input placeholder="Email or mobile number" type={"text"} autoFocus />
+          <Input placeholder="Password" type={"password"} />
+          <ButtonPrimary text="Sign In" type="submit" />
+        </form>
+        <p className="text-center text-xl text-neutral-400 py-4">OR</p>
+        <button className="bg-neutral-600/70 w-full py-2 rounded-sm hover:bg-neutral-700/70 transition-colors duration-150 ease-in-out">
+          Use a sign-in code
+        </button>
+        <p className="text-center py-4">
+          <Link
+            href={"/forgot-password"}
+            className="hover:underline hover:text-neutral-400"
+          >
+            Forgot Password?
+          </Link>
+        </p>
+        <p className="text-neutral-500 font-light">
+          New to Bingewatch?
+          <Link href={"/"} className="text-white font-semibold hover:underline">
+            {" "}
+            Sign up now
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignInForm;
