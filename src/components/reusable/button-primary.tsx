2dc719500a149f5aceca596a467de5fde940344b
ranepaarth <paarth.medisage@outlook.com>
2024-11-02 20:55:59 +0530

    reusable custom components

--- src/components/reusable/button-primary.tsx
+++ src/components/reusable/button-primary.tsx
@@ -1,15 +1,20 @@
 import React, { ButtonHTMLAttributes } from "react";
 
 interface ButtonPrimaryProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  text: string;
+  // text: string;
 }
 
-const ButtonPrimary: React.FC<ButtonPrimaryProps> = ({ className, text }) => {
+const ButtonPrimary: React.FC<ButtonPrimaryProps> = ({
+  className,
+  children,
+  ...props
+}) => {
   return (
     <button
       className={`bg-primary-600 rounded-sm flex py-2 px-4 hover:bg-primary-700  transition-colors duration-200 ease-in-out  justify-center items-center text-nowrap ${className}`}
+      {...props}
     >
-      {text}
+      {children}
     </button>
   );
 };
