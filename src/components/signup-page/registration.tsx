c1a8b99c2d30f49fd789a3388552e7fd97602459
ranepaarth <paarth.medisage@outlook.com>
2024-11-02 21:09:10 +0530

    implement sign in page

--- src/components/signup-page/registration.tsx
+++ src/components/signup-page/registration.tsx
@@ -6,13 +6,13 @@ const Registration = () => {
   return (
     <>
       <div className="flex flex-col gap-4 max-w-[450px] text-center">
-      <StepCount currentStep={1} totalSteps={4} />
+        <StepCount currentStep={1} totalSteps={4} />
         <h3 className="text-4xl font-bold">Finish Setting up your account</h3>
-        <p className="px-20 text-lg">
+        <p className="px-4 md:px-20 text-lg">
           Netflix is personalised for you. Create a password to watch on any
           device at any time.
         </p>
-        <NextButton href={"/signup/regForm"} />
+        <NextButton loading={false}>Next</NextButton>
       </div>
     </>
   );
