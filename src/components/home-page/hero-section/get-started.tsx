b3f4cb97fc58236fc403f453e2cbf3704ce92daa
ranepaarth <paarth.medisage@outlook.com>
2024-11-09 15:14:53 +0530

    refactor get-started functionality

--- src/components/home-page/hero-section/get-started.tsx
+++ src/components/home-page/hero-section/get-started.tsx
@@ -1,22 +1,18 @@
-import { nextConstants } from "@/constants";
-import { getUserEmailFromToken } from "@/server-actions/get-user-email-from-token";
-import { useTranslations } from "next-intl";
+import { getTranslations } from "next-intl/server";
 import React from "react";
 import GetStartedForm from "./get-started-form";
 
-const { BINGEWATCH_SECURE_COOKIE } = nextConstants;
-const GetStarted = async () => {
-  const t = useTranslations("Home.heroSection");
-
-  const decodedToken = await getUserEmailFromToken(BINGEWATCH_SECURE_COOKIE);
+export const dynamic = "force-dynamic";
+const GetStarted = async ({ email, isLoggedIn }: GetStartedData) => {
+  const t = await getTranslations("Home.heroSection");
 
   return (
     <div className="mt-6">
       <GetStartedForm
         placeholder={t("email-placeholder")}
         buttonLabel={t("get-started-btn")}
-        email={decodedToken?.email as string}
-        isLoggedIn={decodedToken?.isLoggedIn as boolean}
+        email={email}
+        isLoggedIn={isLoggedIn}
       />
     </div>
   );
---
+++ src/server-actions/get-user-info-from-token.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import { nextConstants } from "@/constants";
+import { decode } from "next-auth/jwt";
+import { cookies } from "next/headers";
+
+export interface DecodedTokenType {
+  isNewUser?: string;
+  email?: string;
+  isLoggedIn?: boolean;
+}
+
+export const getUserInfoFromToken = async (cookieName: string) => {
+  try {
+    console.log(cookieName);
+    const cookieStore = await cookies();
+    // console.log("after cookies()")
+    const token = cookieStore.get(cookieName);
+    const secret = nextConstants.AUTH_SECRET;
+
+    if (!token) {
+      console.error("No token found in cookies");
+      return { success: true }; // Or handle it according to your logic
+    }
+
+    const decodedToken = (await decode({
+      token: token.value,
+      secret,
+      salt: "10",
+    })) as DecodedTokenType;
+
+    if (!decodedToken) {
+      console.error("Decoded token is null");
+      return { success: true };
+    }
+
+    return decodedToken;
+  } catch (error) {
+    console.error("Error getting user email from token:", error);
+    return { success: true };
+  }
+};
