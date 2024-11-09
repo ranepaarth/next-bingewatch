35e070e4be40779a0aa99ebcaab9f0c7d93ca002
ranepaarth <paarth.medisage@outlook.com>
2024-11-09 15:20:39 +0530

    refactor hero-seciton and faq-section to accept decodedToken props

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,6 +2,11 @@ import FaqSection from "@/components/home-page/faq-section/faq-section";
 import HeroSection from "@/components/home-page/hero-section/hero-section";
 import HomeHeader from "@/components/home-page/home-page-header/home-header";
 import TvSection from "@/components/home-page/tv-section/tv-section";
+import { nextConstants } from "@/constants";
+import {
+  DecodedTokenType,
+  getUserInfoFromToken,
+} from "@/server-actions/get-user-info-from-token";
 import { CodeXml, Github } from "lucide-react";
 import { unstable_setRequestLocale } from "next-intl/server";
 import Link from "next/link";
@@ -13,8 +18,18 @@ type HomeProps = {
   };
 };
 
-const Home = ({ params }: HomeProps) => {
+export const dynamic = "force-dynamic";
+const { BINGEWATCH_SECURE_COOKIE } = nextConstants;
+const Home = async ({ params }: HomeProps) => {
   unstable_setRequestLocale(params.locale);
+  const decodedToken = (await getUserInfoFromToken(
+    BINGEWATCH_SECURE_COOKIE
+  )) as DecodedTokenType;
+
+  // const { email, isNewUser, isLoggedIn } = decodedToken;
+  console.log("-------------");
+  console.log({ decodedToken }, "homePage");
+  console.log("-------------");
 
   return (
     <div className="flex flex-col gap-1 bg-zinc-700 items-center">
@@ -24,12 +39,18 @@ const Home = ({ params }: HomeProps) => {
             <div className="absolute bg-gradient-radial from-black/60 to-black/90 z-10 inset-0"></div>
             <div className="z-20 w-full max-w-screen-xl mx-auto flex flex-col items-center h-full">
               <HomeHeader />
-              <HeroSection />
+              <HeroSection
+                email={decodedToken?.email as string}
+                isLoggedIn={decodedToken?.isLoggedIn as boolean}
+              />
             </div>
           </div>
         </section>
         <TvSection />
-        <FaqSection />
+        <FaqSection
+          email={decodedToken?.email as string}
+          isLoggedIn={decodedToken?.isLoggedIn as boolean}
+        />
       </main>
       <footer className="bg-black w-full">
         <div className="max-w-xl px-4 py-2 text-neutral-600 mx-auto flex justify-between text-sm group">
