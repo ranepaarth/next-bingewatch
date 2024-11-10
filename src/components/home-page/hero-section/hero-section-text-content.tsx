7ebb180ef5fb9ef8550c08e8f008a815f149a7dc
ranepaarth <paarth.medisage@outlook.com>
2024-11-10 12:23:33 +0530

    fix landing page hero-section for sm screens

--- src/components/home-page/hero-section/hero-section-text-content.tsx
+++ src/components/home-page/hero-section/hero-section-text-content.tsx
@@ -5,11 +5,11 @@ const HeroSectionTextContent = () => {
   const t = useTranslations("Home.heroSection");
 
   return (
-    <>
+    <div className="gap-5 flex flex-col px-4">
       <h3 className="text-3xl font-bold lg:text-5xl">{t("h3")} </h3>
       <p className="text-lg lg:text-2xl">{t("p1")}</p>
       <p className="text-lg lg:text-2xl min-w-11/12 max-w-full">{t("p2")}</p>
-    </>
+    </div>
   );
 };
 
