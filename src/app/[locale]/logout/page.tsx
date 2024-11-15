bd2ed3e2420c7f0842e602bdc9c1b45d6c9e18f7
ranepaarth <paarth.medisage@outlook.com>
2024-11-15 13:42:20 +0530

    chore: add env var for api url

--- src/app/[locale]/logout/page.tsx
+++ src/app/[locale]/logout/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
+import { nextConstants } from "@/constants";
 import { useRouter } from "@/navigation";
 import React, { useEffect } from "react";
 
+const { NEXT_APP_URL } = nextConstants;
 export const dynamic = "force-dynamic";
 const LogoutPage = () => {
   const router = useRouter();
   const logoutFunc = async () => {
     try {
-      const response = await fetch(`http://localhost:3000/api/logout`, {
+      const response = await fetch(`${NEXT_APP_URL}/api/logout`, {
         headers: {
           "Content-type": "application/json",
         },
